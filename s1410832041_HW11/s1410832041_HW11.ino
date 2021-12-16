a71875bd9064735973553a558e51f915464fc5ea
leoho0722 <leo160918@gmail.com>
2021-12-16 23:14:42 +0800

    HW11 Commit

--- s1410832041_HW11/s1410832041_HW11.ino
+++ s1410832041_HW11/s1410832041_HW11.ino
@@ -6,22 +6,17 @@
 #include "ESP32Servo.h" // ESP32 用來控制 Speaker 的
 #include "Speaker_Freq.h" // 頻率分配表
 /* 腳位定義 */
-const char DHTPIN = 5; // DHT22: 5
-const char DHTTYPE = DHT22;
-const char LEDs[] = {16, 17, 18, 19}; // LED: 16, 17, 18, 19
-const char Switchs[] = {25, 26, 27}; // Switch1~3: 25, 26, 27
-const char speaker = 33; // Speaker: 33
+const char DHTPIN = 33;
+const char DHTTYPE = DHT11;
+const char LEDs[] = {16, 17, 18, 19};
+const char speaker = 32;
 
 /* Wi-Fi SSID */
 char* ssid = "2706";
 char* pwd = "rootroot";
 
 /* Speaker Settings */
-/* 小蜜蜂1 */
-int freq[] = {262, 294, 330, 349, 392};
-int melody[] = {5,3,3,4,2,2,1,2,3,4,5,5,5,5,3,3,4,2,2,1,3,5,5,3,2,2,2,2,2,3,4,3,3,3,3,3,4,5,5,3,3,4,2,2,1,3,5,5,1};
-int beat[] = {1,1,2,1,1,2,1,1,1,1,1,1,2,1,1,2,1,1,2,1,1,1,1,4,1,1,1,1,1,1,2,1,1,1,1,1,1,2,1,1,2,1,1,2,1,1,1,1,4};
-/* 小蜜蜂2 */
+/* 小蜜蜂 */
 int bee_melody[] = {
     freq_G5, freq_E5, freq_E5, 0, freq_F5, freq_D5, freq_D5, 0, freq_C5, freq_D5, freq_E5, freq_F5, freq_G5, freq_G5, freq_G5, 0,
     freq_G5, freq_E5, freq_E5, 0, freq_F5, freq_D5, freq_D5, 0, freq_C5, freq_E5, freq_G5, freq_G5, freq_E5, 0, 0, 0,
@@ -56,7 +51,7 @@ WebServer server(80);
 void setup() {
   dht.begin();
   Serial.begin(115200);
-  // SerialBT.begin("ESP32Test");
+//  SerialBT.begin("ESP32Test");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pwd);
   while (WiFi.status() != WL_CONNECTED) { delay(500); }
@@ -74,9 +69,6 @@ void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(LEDs[i], OUTPUT);
     digitalWrite(LEDs[i], LOW);
-  }
-  for (int i = 0; i < 3; i++) {
-    pinMode(Switchs[i], INPUT);
   }
   pinMode(speaker, OUTPUT);
 }
@@ -104,7 +96,7 @@ void systemRoute() {
     if (temp > 25.00) {
       dhtState = dhtState + "，室內溫度有變高的趨勢喔";
       digitalWrite(LEDs[0], HIGH); // LEDs[0]：提醒室內溫度有變高的趨勢
-      littleBee2();
+      littleBee();
     } else if (temp < 20.00) {
       dhtState = dhtState + "，室內溫度有變低的趨勢喔";
       digitalWrite(LEDs[1], HIGH); // LEDs[1]：提醒室內溫度有變低的趨勢
@@ -115,7 +107,7 @@ void systemRoute() {
     if (humidity > 65.00) {
       dhtState = dhtState + "，室內濕度有點高喔";
       digitalWrite(LEDs[2], HIGH); // LEDs[2]：提醒室內濕度有變高的趨勢
-      littleBee2();
+      littleBee();
     } else if (humidity < 45.00) {
       dhtState = dhtState + "，室內濕度有點低喔";
       digitalWrite(LEDs[3], HIGH); // LEDs[3]：提醒室內濕度有變低的趨勢
@@ -126,8 +118,7 @@ void systemRoute() {
   }
   Serial.println(dhtState);
   HTML = HTML + dhtState + HTML_Footer;
-  Serial.println(HTML);
-  server.send(200, "text/html", HTML); // 處理顯示溫溼度狀態的路由
+  server.send(200, "text/html", HTML);
 }
 
 // 溫溼度讀取
@@ -146,28 +137,19 @@ void getDHT() {
   Serial.print(humidity);
   Serial.println("%");
   // SerialBT Output
-  SerialBT.print("Temperature: ");
-  SerialBT.print(temp);
-  SerialBT.println("Degree C");
-  SerialBT.print("Humidity: ");
-  SerialBT.print(humidity);
-  SerialBT.println("%");
+//  SerialBT.print("Temperature: ");
+//  SerialBT.print(temp);
+//  SerialBT.println("Degree C");
+//  SerialBT.print("Humidity: ");
+//  SerialBT.print(humidity);
+//  SerialBT.println("%");
   delay(1000);
 }
 
 // Speaker 發聲音
 // 小蜜蜂－溫溼度變高的時候放
 void littleBee() {
-  for (int i = 0; i < 49; i++) {
-    tone(speaker, freq[melody[i]-1]);
-    delay(beat[i]*250);
-    noTone(speaker);
-    delay(0);
-  }
-  delay(1000);
-}
-void littleBee2() {
-  for(int i = 0; i < sizeof(bee_melody)/sizeof(int); i++){
+  for(int i = 0; i < sizeof(bee_melody)/sizeof(int); i++) {
     tone(speaker, bee_melody[i], 55 * bee_beat[i]);
     delay(75 * bee_beat[i]);
   }  
@@ -175,7 +157,7 @@ void littleBee2() {
 
 // 小星星－溫溼度變低的時候放
 void littleStar() {
-  for(int i = 0; i < sizeof(bee_melody)/sizeof(int); i++){
+  for(int i = 0; i < sizeof(bee_melody)/sizeof(int); i++) {
     tone(speaker, star_melody[i], 55 * star_beat[i]);
     delay(75 * star_beat[i]);
   }
